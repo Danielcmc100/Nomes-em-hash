72d01d927cd0a5c95a1d769cc28f85bbc9f4f6c2
danielcmc100 <danielcmc100@gmail.com>
2024-06-11 19:04:12 -0300

    feat: Add menu options for collision treatments and hash functions
    This commit adds menu options for collision treatments and hash functions in the main.py file. The menu allows the user to choose between different collision treatments (Encadeamento Exterior, Encadeamento Interior, Endereçamento Aberto) and hash functions (Divisão, Dobra, Multiplicação). The selected options are then used to perform specific actions in the program.

--- main.py
+++ main.py
@@ -23,20 +23,46 @@ from menu import ShowMenu
 # 9. Exiba a tabela hash atualizada apos cada operacao.
 
 
+def main():
+    global tabela_hash
+
+    menu_funcao_hash()
+    menu_tratamento_de_colisoes()
+
+    tamanho_tabelha_hash = int(ShowMenu([], "Informe o tamanho da tabela"))
+    tabela_hash = TabelaHash(tamanho_tabelha_hash)
+    menu()
+
+opcao_tratamento_de_colisoes = None
+opcao_funcao_hash = None
+
+
 class TabelaHash:
-    def __init__(self, tamanho, metodo_dispersao):
+    def __init__(self, tamanho):
         # substitui o dicionario por uma lista, como nao da pra criar um
         # vertor vazio em python, criei uma lista de tamanho "tamanho" e
         # preenchi ela com "None" (do mesmo jeito que a professora fez)
         self.tabela = [None] * tamanho
         self.tamanho = tamanho
-        self.metodo_dispersao = metodo_dispersao
-        
+
 
     def adicionar(self, nome):
         indice = self.calcula_index(nome)
-        self.tabela[indice] = nome
-    
+        
+        if self.tabela[indice] is None:
+            self.tabela[indice] = nome
+        else:
+            match opcao_tratamento_de_colisoes:
+                case 1:
+                        pass
+                case 2:
+                        pass
+                case 3:
+                        pass
+                        
+                case _:
+                    raise ValueError("Opção de tratamento de colisão inválida")
+
 
     # Se a pesquisa encontrar resultados, retorna indice em que
     # o nome está localizado
@@ -64,19 +90,23 @@ class TabelaHash:
 
     def hash_multiplicacao(self, nome):
         pass
-    
-    
+
     def calcula_index(self, nome):
-        match self.metodo_dispersao:
-            case 'divisao':
+        match opcao_funcao_hash:
+            case 1:
                 return self.hash_divisao(nome)
+            case 2:
+                return self.hash_dobra(nome)
+            case 3:
+                return self.hash_multiplicacao(nome)
+            case _:
+                raise ValueError("Opção de hash inválida")
 
 
-opcao_tratamento_de_colisoes = None
-opcao_funcao_hash = None
 
-def tratamento_de_colisoes():
+def menu_tratamento_de_colisoes():
     while True:
+        global opcao_tratamento_de_colisoes
         tratamentos_de_colisoes = ["Encadeamento Exterior", "Encadeamento Interior", "Endereçamento Aberto"]
         opcao_tratamento_de_colisoes = ShowMenu(tratamentos_de_colisoes, "Escolha um tratamento de colisões")
         match opcao_tratamento_de_colisoes:
@@ -93,14 +123,14 @@ def tratamento_de_colisoes():
                 pass
 
 
-def funcao_hash():
+def menu_funcao_hash():
     while True:
+        global opcao_funcao_hash
         funcaoes_hash = ["Divisão", "Dobra", "Multiplicação"]
         opcao_funcao_hash = ShowMenu(funcaoes_hash, "Escolha mua função hash")
         match opcao_funcao_hash:
             case "1":
-                return 'divisao'
-                break
+                pass
             case "2":
                 # Encadeamento Interior
                 pass
@@ -128,13 +158,13 @@ def menu():
 
 
 def adicionar():
-    nome = input('insira o nome que deseja adicionar na tabela: ')
+    nome = ShowMenu([], "Nome que deseja adicionar na tabela")
     tabela_hash.adicionar(nome)
     print(f'{nome} adicionado com sucesso!')
 
 
 def buscar():
-    nome = input('insira o nome que deseja adicionar na tabela: ')
+    nome = ShowMenu([], "Nome que deseja buscar na tabela")
     pesquisa = tabela_hash.pesquisa(nome)
     if pesquisa is False:
         print(f'O nome "{nome}" não foi encontrado!')
@@ -143,20 +173,13 @@ def buscar():
 
 
 def remover():
-    nome = input('insira o nome que deseja remover tabela: ')
+    nome = ShowMenu([], "Nome que deseja remover na tabela")
     pesquisa = tabela_hash.pesquisa(nome)
     if pesquisa is False:
         print(f'O nome "{nome}" fão encontrado!')
     else:
         tabela_hash.remover(nome)
         print(f"O {nome} foi removido da tabela!")
-
-
-# Removi a funcao main() para que "tabela_hash" seja global
-tamanho = int(input('Informe o tamanho da tabela: '))
-dispersao = funcao_hash()
-tabela_hash = TabelaHash(tamanho, dispersao)
-menu()
 
 
 # Exemplo de switch case em Python
@@ -171,11 +194,5 @@ menu()
 #         action-default
 
 
-# if __name__ == '__main__':
-#     main()
-
-
-
-    
-
-    
+if __name__ == '__main__':
+    main()
